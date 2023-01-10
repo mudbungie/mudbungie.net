03293be2b37ca26fb69335ce82bfd08172105f3d
mudbungie <mudbungie@gmail.com>
2023-01-09 19:38:31 -0800

    Stuff returns response

--- webserver.py
+++ webserver.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from pages.root.root import render_root
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/', response_class=HTMLResponse)
 def root():
-    return render_root()
-    
+    content = render_root()
+    response = HTMLResponse(content=content, status_code=200)
